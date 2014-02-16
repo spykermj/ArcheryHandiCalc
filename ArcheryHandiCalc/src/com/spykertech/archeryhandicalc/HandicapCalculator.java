10b10556ee4ebedf6771965c49ca7cb236733c77
spykermj <jeremy.spykerman@gmail.com>
2014-02-16 07:41:18 -0500

    Functioning FITA ten zone average score Functioning Imperial average score

--- ArcheryHandiCalc/src/com/spykertech/archeryhandicalc/HandicapCalculator.java
+++ ArcheryHandiCalc/src/com/spykertech/archeryhandicalc/HandicapCalculator.java
@@ -1,18 +1,35 @@
 package com.spykertech.archeryhandicalc;
 
+import android.util.Log;
+
 public class HandicapCalculator {
 
 	public static double yardsToMeters(double yards) {
 		return yards * 0.9144;
 	}
 
-	public static double getRMS(int handicap, int range, boolean rangeIsYards) {
-		double dRange = range;
-		double dHandicap = handicap;
+	public static double getRMS(double handicap, double range, boolean rangeIsYards) {
 		if(rangeIsYards){
-			dRange = yardsToMeters(range);
+			range = yardsToMeters(range);
 		}
-		//  100 * range * 1.036^(handicap + 12.9) * 5 * (10^-4) * (1 + 1.429 * 10^-6 * 1.07^(handicap + 4.3) * range^2)
-		return 100 * dRange * Math.pow(1.036, (dHandicap + 12.9)) * 5 * (Math.pow(10.0, -4.0)) * (1 + 1.429 * Math.pow(10.0, -6.0) * Math.pow(1.07, (dHandicap + 4.3)) * Math.pow(dRange, 2));
+		return 100 * range * Math.pow(1.036, (handicap + 12.9)) * 5 * (Math.pow(10.0, -4.0)) * (1 + 1.429 * Math.pow(10.0, -6.0) * Math.pow(1.07, (handicap + 4.3)) * Math.pow(range, 2));
+	}
+
+	public static double getFITATenZoneAverageScore(double handicap, double range, double faceDiameter, boolean rangeIsYards) {
+		double sum = 0;
+		double rms = getRMS(handicap, range, rangeIsYards);
+		for(int i=1; i<=10; i++) {
+			sum += Math.exp(-Math.pow((i * faceDiameter / 20 + 0.357), 2) / Math.pow(rms, 2));
+		}
+		return 10 - sum;
+	}
+
+	public static double getImperialAverageScore(double handicap, double range, double faceDiameter, boolean rangeIsYards) {
+		double sum = 0;
+		double rms = getRMS(handicap, range, rangeIsYards);
+		for(int i=1; i<=4; i++){
+			sum += Math.exp(-Math.pow(i * faceDiameter / 10 + 0.357, 2) / Math.pow(rms, 2));
+		}
+		return 9 - 2 * sum - Math.exp(-Math.pow(faceDiameter / 2 + 0.357, 2) / Math.pow(rms, 2));
 	}
 }
