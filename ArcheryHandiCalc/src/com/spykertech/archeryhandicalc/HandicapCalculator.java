ac3719381348799c1bd3aa74e00d89755b9b01bf
spykermj <jeremy.spykerman@gmail.com>
2014-02-16 07:56:38 -0500

    FITA five zone average score

--- ArcheryHandiCalc/src/com/spykertech/archeryhandicalc/HandicapCalculator.java
+++ ArcheryHandiCalc/src/com/spykertech/archeryhandicalc/HandicapCalculator.java
@@ -1,7 +1,5 @@
 package com.spykertech.archeryhandicalc;
 
-import android.util.Log;
-
 public class HandicapCalculator {
 
 	public static double yardsToMeters(double yards) {
@@ -31,5 +29,14 @@ public class HandicapCalculator {
 			sum += Math.exp(-Math.pow(i * faceDiameter / 10 + 0.357, 2) / Math.pow(rms, 2));
 		}
 		return 9 - 2 * sum - Math.exp(-Math.pow(faceDiameter / 2 + 0.357, 2) / Math.pow(rms, 2));
+	}
+
+	public static double getFITAFiveZoneAverageScore(double handicap, double range, double faceDiameter, boolean rangeIsYards) {
+		double sum = 0;
+		double rms = getRMS(handicap, range, rangeIsYards);
+		for(int i=1; i<=4; i++){
+			sum += Math.exp(-Math.pow(i * faceDiameter / 20 + 0.357, 2) / Math.pow(rms, 2));
+		}
+		return 10 - sum - 6 * Math.exp(-Math.pow(5 * faceDiameter / 20 + 0.357, 2));
 	}
 }
